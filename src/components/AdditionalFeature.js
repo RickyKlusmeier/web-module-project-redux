ca801a8f21d10acea39dfe084b63f7a5e3a8ddf7
Ricky Klusmeier <RickyKlusmeier@gmail.com>
2021-04-14 14:26:11 -0600

    Update AdditionalFeature.js

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { addFeature } from '../actions/actions'
 
+
+
+const AdditionalFeature = props => {
+  return (
+    <li>
+      {/* Add an onClick that will let you add a feature to your car */}
+      <button className="button" onClick={() => props.addFeat(props.feature)}>Add</button>
+      {props.feature.name} (+{props.feature.price})
+    </li>
+  );
+};
+
 const mapStateToProps = (state) => {
   return {
     car: state.car,
@@ -15,15 +27,5 @@ const mapDispatchToState = (dispatch) => {
     addFeat: (feature) => dispatch(addFeature(feature))
   }
 }
-
-const AdditionalFeature = props => {
-  return (
-    <li>
-      {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button" onClick={() => props.addFeat(props.feature)}>Add</button>
-      {props.feature.name} (+{props.feature.price})
-    </li>
-  );
-};
 
 export default connect(mapStateToProps, mapDispatchToState)(AdditionalFeature);
